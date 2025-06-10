2506f6b871174a71a71cc3c80cb3c320f00c349a
Lumorn <mail@daniel-thermann.de>
2025-06-10 23:08:18 +0200

    Add DevTools button and integrate toggle

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,12 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron');
 const path = require('path');
 const fs = require('fs');
+
+// Flag, ob die DevTools beim Start geöffnet werden sollen
+const isDebug = process.argv.includes('--debug');
+
+// Referenz auf das Hauptfenster speichern
+let mainWindow;
 
 // Pfade zu EN und DE relativ zur HTML-Datei
 
@@ -22,7 +28,7 @@ function readAudioFiles(dir) {
 }
 
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
@@ -30,7 +36,21 @@ function createWindow() {
     },
   });
 
-  win.loadFile(path.join(__dirname, '..', 'hla_translation_tool.html'));
+  mainWindow.loadFile(path.join(__dirname, '..', 'hla_translation_tool.html'));
+
+  // DevTools optional öffnen, wenn das Flag gesetzt ist
+  if (isDebug) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  // Shortcut zum Ein- und Ausblenden der DevTools
+  globalShortcut.register('CommandOrControl+Shift+I', () => {
+    if (mainWindow.webContents.isDevToolsOpened()) {
+      mainWindow.webContents.closeDevTools();
+    } else {
+      mainWindow.webContents.openDevTools();
+    }
+  });
 }
 
 app.whenReady().then(() => {
@@ -48,7 +68,22 @@ app.whenReady().then(() => {
     };
   });
 
+  // DevTools auf Befehl aus dem Renderer umschalten
+  ipcMain.on('toggle-devtools', () => {
+    if (!mainWindow) return;
+    if (mainWindow.webContents.isDevToolsOpened()) {
+      mainWindow.webContents.closeDevTools();
+    } else {
+      mainWindow.webContents.openDevTools();
+    }
+  });
+
   createWindow();
+
+  // Beim Beenden alle Shortcuts wieder freigeben
+  app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+  });
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
