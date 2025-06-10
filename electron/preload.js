11d36f88582fc0050c7f63a73cff258c7d072ddf
Lumorn <mail@daniel-thermann.de>
2025-06-10 23:14:28 +0200

    Add DevTools toggle button

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   scanFolders: () => ipcRenderer.invoke('scan-folders'),
+  // Befehl an Hauptprozess senden, um DevTools umzuschalten
+  toggleDevTools: () => ipcRenderer.send('toggle-devtools'),
 });
