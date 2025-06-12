ff4a1b922443fa32da918dbe803af13496060976
Lumorn <mail@daniel-thermann.de>
2025-06-12 22:13:00 +0200

    Verbessere Fehlerprotokoll und Versionsnummer

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ let undoStack          = [];
 let redoStack          = [];
 
 // Version wird zur Laufzeit ersetzt
-const APP_VERSION = '1.18.1';
+const APP_VERSION = '1.18.2';
 
 // =========================== GLOBAL STATE END ===========================
 
